e98e600050f00a9f866a59ce98f8cb20508b121a
elefert400 <elefert400@gmail.com>
2019-05-07 10:34:49 -0500

    got some work into the CPU class

---
+++ CPU.cpp
@@ -0,0 +1,164 @@
+#include "CPU.h"
+
+using namespace std;
+
+CPU::CPU()
+{
+  p0transfers = 0;
+  p1transfers = 0;
+  p2transfers = 0;
+  p3transfers = 0;
+
+  dirtyWrite = 0;
+
+  m = 0;
+  o = 0;
+  s = 0;
+  i = 0;
+
+  for(int i = 0; i < arraySize; i++)
+  {
+    states[i] = "";
+    tag[i] = "";
+  }
+}
+
+CPU::~CPU()
+{
+
+}
+
+void CPU::exclusiveToModified();
+{
+
+}
+
+void CPU::exclusiveToInvalid();
+{
+
+}
+
+void CPU::exclusiveToShared()
+{
+
+}
+
+void CPU::exclusiveToExclusve()
+{
+
+}
+
+void CPU::modifiedToOwner();
+{
+
+}
+
+void CPU::modifiedToInvalid()
+{
+
+}
+
+void CPU::modifiedToModified()
+{
+
+}
+
+void CPU::ownerToOwner()
+{
+
+}
+
+void CPU::ownerToModifed()
+{
+
+}
+
+void CPU::ownerToInvalid()
+{
+
+}
+
+void CPU::sharedToShared()
+{
+
+}
+
+void CPU::sharedToModified()
+{
+
+}
+
+void CPU::sharedToInvalid()
+{
+
+}
+
+void CPU::invalidToModified()
+{
+
+}
+
+void CPU::invalidToExclusive()
+{
+
+}
+
+void CPU::invalidToShared()
+{
+
+}
+
+bool CPU::find(string findMe)
+{
+
+}
+
+string CPU::stateIn(int index)
+{
+
+}
+
+int CPU::getp0transfers()
+{
+  return p0transfers;
+}
+
+int CPU::getp1transfers()
+{
+  return p1transfers;
+}
+
+int CPU::getp2transfers()
+{
+  return p2transfers;
+}
+
+int CPU::getp3transfers()
+{
+  return p3transfers;
+}
+
+int CPU::getDirtyWrite()
+{
+  return dirtyWrite;
+}
+
+int CPU::getm()
+{
+  return m;
+}
+
+int CPU::geti()
+{
+  return i;
+}
+
+int CPU::geto()
+{
+  return o;
+}
+
+int CPU::gets()
+{
+  return s;
+}
---
+++ CPU.h
@@ -0,0 +1,65 @@
+#idndef CPU_H
+#define CPU_H
+
+class CPU
+{
+private:
+  int p0transfers;
+  int p1transfers;
+  int p2transfers;
+  int p3transfers;
+
+  //need question 2 variables
+
+  int dirtyWrite;
+
+  int m;
+  int o;
+  int s;
+  int i;
+
+  int arraySize = 512;
+  string states[arraySize];
+  string tag[arraySize];
+public:
+  //constructor destructor
+  CPU();
+  ~CPU();
+  //state change functions
+  void exclusiveToModified();
+  void exclusiveToInvalid();
+  void exclusiveToShared();
+  void exclusiveToExclusve();
+
+  void modifiedToOwner();
+  void modifiedToInvalid();
+  void modifiedToModified();
+
+  void ownerToOwner();
+  void ownerToModifed();
+  void ownerToInvalid();
+
+  void sharedToShared();
+  void sharedToModified();
+  void sharedToInvalid();
+
+  void invalidToModified();
+  void invalidToExclusive();
+  void invalidToShared();
+
+  //functions to help get info from the CPU
+  bool find(string findMe);
+  string stateIn(int index);
+
+  //gets the final variables for output
+  int getp0transfers();
+  int getp1transfers();
+  int getp2transfers();
+  int getp3transfers();
+  int getDirtyWrite();
+  int getm();
+  int geti();
+  int geto();
+  int gets();
+};
+#endif
